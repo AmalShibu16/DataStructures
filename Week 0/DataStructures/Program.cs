0b4210824bc9b20204af0fe3a5aa0c333b32fd8c
AmalShibu16 <amalshibu16@gmail.com>
2024-09-25 17:26:30 +0100

    Final

--- Week 0/DataStructures/Program.cs
+++ Week 0/DataStructures/Program.cs
@@ -1,35 +1,75 @@
 ﻿using DataStructures;
 
-namespace datastructures
+namespace DataStructures
 {
-    internal class program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            MainMenu();
+
+            Console.ReadKey();
+        }
+
+        public static void ListMovies()
+        {
             Movie deadpool3;
             deadpool3.title = "deadpool & wolverine";
-            deadpool3.releaseyear = 2024;
+            deadpool3.releaseYear = 2024;
             deadpool3.price = 19.99;
             deadpool3.rating = MovieRating._15;
 
-            Console.WriteLine(deadpool3.title + "(" + deadpool3.releaseyear + ") rated" + deadpool3.rating);
+            Console.Clear();
+            Console.WriteLine(deadpool3.title + "(" + deadpool3.releaseYear + ") Rated " + deadpool3.rating);
+        }
 
-            Console.ReadKey();
-            Console.WriteLine("\n\n");
-
-            //Console.Clear();
-
+        public static void ListShows()
+        {
             List<TVShow> shows = new List<TVShow>();
             shows.Add(new TVShow("The Lord of the Rings: The Rings of Power", 2022, 0, MovieRating._15));
             shows.Add(new TVShow("The Boys", 2019, 7.99, MovieRating._18));
             shows.Add(new TVShow("Ben and Holly's Little Kingdom", 2022, 0, MovieRating._U));
 
+            Console.Clear();
+
             for (int i = 0; i < shows.Count; i++)
             {
                 Console.WriteLine(shows[i].ToString());
             }
+        }
 
-            Console.ReadKey();
+        public static void MainMenu()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Please choose from the following options : \n");
+                Console.WriteLine("(1) List Movies");
+                Console.WriteLine("(2) List TVShows");
+                Console.WriteLine("\n");
+                Console.WriteLine("(0) Exit Application");
+                Console.WriteLine("\n");
+
+                string userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out int integerInput))
+                {
+                    switch (integerInput)
+                    {
+                        case 0:
+                            Environment.Exit(0);
+                            break;
+                        case 1:
+                            ListMovies();
+                            Console.ReadKey();
+                            break;
+                        case 2:
+                            ListShows();
+                            Console.ReadKey();
+                            break;
+                    }
+                }
+            }
         }
 
         public enum MovieRating
@@ -40,17 +80,14 @@ namespace datastructures
             _15,
             _18
         }
-
         public struct Movie
         {
             public string title;
-            public int releaseyear;
+            public int releaseYear;
             public double price;
             public MovieRating rating;
         }
 
     }
-
-
 
 }
--- Week 0/DataStructures/TVShow.cs
+++ Week 0/DataStructures/TVShow.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static datastructures.program;
+using static DataStructures.Program;
 
 namespace DataStructures
 {
